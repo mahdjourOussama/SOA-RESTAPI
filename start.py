4dfff486cf76d02ff822250144502e7b930375d2
Mahdjour Oussama <76874664+mahdjourOussama@users.noreply.github.com>
2024-11-16 12:20:35 +0100

    add start services

---
+++ start.py
@@ -0,0 +1,32 @@
+import subprocess
+import signal
+import sys
+
+services = [
+    "WatchdogService.py",
+    "DecisionApprovalService.py",
+    "ProperityEvaluationService.py",
+    "ServiceComposer.py",
+    "SolvapilityVerificationService.py",
+    "TextMiningService.py",
+]
+
+processes = []
+
+
+def signal_handler(sig, frame):
+    for process in processes:
+        process.terminate()
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    for service in services:
+        process = subprocess.Popen(["python", service])
+        processes.append(process)
+
+    for process in processes:
+        process.wait()
